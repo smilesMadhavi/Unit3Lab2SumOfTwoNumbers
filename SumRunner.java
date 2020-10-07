0de6a7ec67958a9968bcdc4ad234dd8560a27429
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-10-07 10:02:57 -0500

    Add files via upload

---
+++ SumRunner.java
@@ -0,0 +1,23 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+
+public class SumRunner
+{
+	public static void main( String[] args )
+	{
+		Sum test = new Sum();
+		test.setNums(5,5);
+		test.sum();
+		test.print();
+
+		test.setNums(90,100.0);
+		test.sum();
+		test.print();
+		
+		//add more test cases
+				
+	}
+}
